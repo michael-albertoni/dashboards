953f679ba7b2694f2f6b240ced584ae1c0a6870c
Michael <michael.albertoni@hotmail.com>
2022-11-03 12:16:27 -0300

    Correção de gramática

--- callback_multiplo.py
+++ callback_multiplo.py
@@ -18,12 +18,12 @@ app.layout = html.Div([
     html.Div([
         html.Div([
             dcc.Dropdown(
-                id='xaxis-column',
+                id='xaxes-column',
                 options=[{'label': i, 'value': i} for i in indicators],
                 value='Fertility rate, total (births per woman)'
             ),
             dcc.RadioItems(
-                id='xaxis-type',
+                id='xaxes-type',
                 options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
                 value='Linear',
                 labelStyle={'display': 'inline-block'}
@@ -33,12 +33,12 @@ app.layout = html.Div([
 
         html.Div([
             dcc.Dropdown(
-                id='yaxis-column',
+                id='yaxes-column',
                 options=[{'label': i, 'value': i} for i in indicators],
                 value='Life expectancy at birth, total (years)'
             ),
             dcc.RadioItems(
-                id='yaxis-type',
+                id='yaxes-type',
                 options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
                 value='Linear',
                 labelStyle={'display': 'inline-block'}
@@ -61,25 +61,25 @@ app.layout = html.Div([
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value'),
-     Input('xaxis-type', 'value'),
-     Input('yaxis-type', 'value'),
+    [Input('xaxes-column', 'value'),
+     Input('yaxes-column', 'value'),
+     Input('xaxes-type', 'value'),
+     Input('yaxes-type', 'value'),
      Input('year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name, xaxis_type, yaxis_type, year_value):
+def update_graph(xaxes_column_name, yaxes_column_name, xaxes_type, yaxes_type, year_value):
     nova_df=df[df['Year']==year_value]
 
-    fig = px.scatter(x=nova_df[nova_df['Indicator Name'] == xaxis_column_name]['Value'],
-                     y=nova_df[nova_df['Indicator Name'] == yaxis_column_name]['Value'],
-                     hover_name=nova_df[nova_df['Indicator Name'] == yaxis_column_name]['Country Name'])
+    fig = px.scatter(x=nova_df[nova_df['Indicator Name'] == xaxes_column_name]['Value'],
+                     y=nova_df[nova_df['Indicator Name'] == yaxes_column_name]['Value'],
+                     hover_name=nova_df[nova_df['Indicator Name'] == yaxes_column_name]['Country Name'])
 
     fig.update_layout(margin={'l':40, 'b': 40, 't':10, 'r':0}, hovermode='closest')
 
-    fig.update_xaxes(title=xaxis_column_name,
-                     type='linear' if xaxis_type == 'Linear' else 'log')
+    fig.update_xaxes(title=xaxes_column_name,
+                     type='linear' if xaxes_type == 'Linear' else 'log')
 
-    fig.update_yaxes(title=yaxis_column_name,
-                     type='linear' if yaxis_type == 'Linear' else 'log')
+    fig.update_yaxes(title=yaxes_column_name,
+                     type='linear' if yaxes_type == 'Linear' else 'log')
 
     return fig
 
