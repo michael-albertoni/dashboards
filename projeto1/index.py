a1e1e04b7739f78ac8f5f319636fd0967196c26e
Michael <michael.albertoni@hotmail.com>
2022-11-08 16:50:38 -0300

    finaliza projeto 1 com os callbacks

--- projeto1/index.py
+++ projeto1/index.py
@@ -38,8 +38,40 @@ app.layout = html.Div(children=[
 
 
 #Callbacks
+@app.callback(
+    [
+        Output('city_fig', 'figure'),
+        Output('pay_fig', 'figure'),
+        Output('income_per_product_fig', 'figure')
+    ],
+    [    
+        Input('check_city', 'value'),
+        Input('main_variable', 'value')
+    ]   
+)
+def render_graphs(cities, main_variable):
+    #define a operação a ser realizada com os dados se vai usar soma ou média de acordo com o radio selecionado
+    operation = np.sum if main_variable == 'gross income' else np.mean
+    
+    #filtra cidades
+    df_filtered = df_data[df_data['City'].isin(cities)]
+    
+    #selecionar colunas
+    df_city = df_filtered.groupby("City")[main_variable].apply(operation).to_frame().reset_index()
+    df_payment = df_filtered.groupby("Payment")[main_variable].apply(operation).to_frame().reset_index()
+    df_product_income = df_filtered.groupby(["Product line","City"])[main_variable].apply(operation).to_frame().reset_index()
 
+    #criar figuras dos gráficos
+    fig_city = px.bar(df_city, x='City', y=main_variable)
+    fig_payment = px.bar(df_payment, y='Payment', x=main_variable, orientation='h')
+    fig_product_income = px.bar(df_product_income, x=main_variable, y='Product line', color="City", orientation='h', barmode='group')
 
+    #alteração de layout
+    fig_city.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200)
+    fig_payment.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=200)
+    fig_product_income.update_layout(margin=dict(l=0,r=0,t=20,b=20), height=500)
+
+    return fig_city, fig_payment, fig_product_income
 
 if __name__ == '__main__':
     app.run_server(port=8050, debug=True)
