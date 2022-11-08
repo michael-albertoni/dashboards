7e00743c8c17ca14d90ec7ae7ca0971da3b320d5
Michael <michael.albertoni@hotmail.com>
2022-11-08 15:24:05 -0300

    atualiza estrutura do projeto com inputs e gráficos ainda não funcionais

--- projeto1/index.py
+++ projeto1/index.py
@@ -15,12 +15,24 @@ server = app.server
 df_data = pd.read_csv('supermarket_sales.csv')
 #ajusta coluna de datas para não ficar como string e sim como data
 df_data['Date'] = pd.to_datetime(df_data['Date'])
-
+cidades = df_data["City"].value_counts().index
 
 
 
 #Layout
 app.layout = html.Div(children=[
+    html.H5("Cidades:"),
+    #checklist primeiro a lista, e depois a lista com os marcados, id para acessar pelo Callback
+    dcc.Checklist(cidades, cidades, id='check_city'),
+
+    html.H5("Variável de análise:"),
+    #radio buttons, primeiro a lista, e depois o item padrão selecionado
+    dcc.RadioItems(["gross income", "Rating"],  "gross income", id="main_variable"),
+
+    #gráficos criados pelos callbacks
+    dcc.Graph(id='city_fig'),
+    dcc.Graph(id='pay_fig'),
+    dcc.Graph(id='income_per_product_fig'),
 
 ])
 
